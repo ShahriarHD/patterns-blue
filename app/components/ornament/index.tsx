0b12038e24ce40247cf84a61620d60289a66ba33
Shahriar <shahriarheshmat@gmail.com>
2022-03-23 14:35:32 +0430

    create ornament component

---
+++ app/components/ornament/index.tsx
@@ -0,0 +1,68 @@
+import cx from 'classnames';
+import { HTMLAttributes, PropsWithChildren, useCallback, useState } from 'react';
+import { Link, LinkProps } from 'remix';
+import add from './add-min.png';
+import error from './error-min.png';
+import loading from './loading-min.png';
+import moon from './moon-min.png';
+import rainbowFlower from './rainbow-flower-min.png';
+import sun from './sun-min.png';
+
+export declare type OrnamentSize = 'sm' | 'md' | 'lg';
+
+export declare type OrnamentDecor = 'rainbow-flower' | 'moon' | 'sun' | 'loading' | 'error' | 'add';
+
+declare type OrnamentButtonProps = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
+
+declare type OrnamentLinkProps = LinkProps;
+
+declare type OrnamentUnfoldingChildrenProps = PropsWithChildren<{
+    isUnfolded: boolean
+}>
+
+declare type OrnamentSharedProps = {
+    decoration: OrnamentDecor,
+    size?: OrnamentSize,
+    behavior?: 'idle' | 'spinning',
+    className?: string,
+};
+
+function getOrnamentAsset(type: OrnamentDecor) {
+    let asset;
+    switch (type) {
+        case 'moon': { asset = moon; break; }
+        case 'sun': { asset = sun; break; }
+        case 'rainbow-flower': { asset = rainbowFlower; break; }
+        case 'error': { asset = error; break; }
+        case 'loading': { asset = loading; break; }
+        case 'add': { asset = add; break; }
+    }
+
+    return asset;
+}
+
+export const Ornament = {
+    Image({ type }: { type: OrnamentDecor }) {
+        const asset = getOrnamentAsset(type);
+        return <img src={asset} />
+    },
+    Link(props: OrnamentSharedProps & OrnamentLinkProps) {
+        const { decoration: type, size, behavior, className: givenClassName, ...rest } = props;
+        const className = cx('ornament', behavior, size, givenClassName)
+        return (
+            <Link {...rest} className={className}>
+                <Ornament.Image type={type} />
+            </Link>
+        )
+    },
+    Button(props: OrnamentSharedProps & OrnamentButtonProps) {
+        const { decoration: type, size, behavior, className: givenClassName, ...rest } = props;
+
+        const className = cx('ornament', behavior, size, givenClassName)
+        return (
+            <button type="button" className={className} {...rest}>
+                <Ornament.Image type={type} />
+            </button>
+        )
+    }
+}
